4f932ec2ab6dc0dfbc606b50290f74d8824efbc4
kernelfault1024 <141175936+kernelfault1024@users.noreply.github.com>
2023-12-06 09:20:29 -0800

    Add files via upload

---
+++ DeploymentScript.ps1
@@ -0,0 +1,11 @@
+﻿##AD DS install and join to existing domain
+Add-Computer -DomainName ReinerChiro.local -Credential ReinerChiro\oit -Verbose -Restart -Force
+
+Install-WindowsFeature AD-Domain-Services -IncludeManagementTools
+Install-ADDSDomainController -DomainName 'ReinerChiro.local' -InstallDns -Credential (Get-Credential 'ReinerChiro\oit') -SafeModeAdministratorPassword (Get-Credential 'SafeModeAdministratorPassword').Password -NoRebootOnCompletion:$false
+
+
+
+
+Install-WindowsFeature DNS -IncludeManagementTools
+Install-WindowsFeature DHCP -IncludeManagementTools
