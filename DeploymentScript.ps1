f4c8991edd538fa86084067151154f83fb02c9db
kernelfault1024 <141175936+kernelfault1024@users.noreply.github.com>
2023-12-11 10:52:26 -0800

    Commit 1211

--- DeploymentScript.ps1
+++ DeploymentScript.ps1
@@ -2,7 +2,7 @@
 Add-Computer -DomainName ReinerChiro.local -Credential ReinerChiro\oit -Verbose -Restart -Force
 ##Install ADDS feature
 Install-WindowsFeature AD-Domain-Services -IncludeManagementTools
-##Install as a domain controller
+##Install as a domain controller, make sure the oit account is an enterprise admin
 Install-ADDSDomainController -DomainName 'ReinerChiro.local' -InstallDns -Credential (Get-Credential 'ReinerChiro\oit') -SafeModeAdministratorPassword (Get-Credential 'SafeModeAdministratorPassword').Password -NoRebootOnCompletion:$false
 
 
@@ -13,10 +13,13 @@ Install-WindowsFeature DNS -IncludeManagementTools
 ##Install DHCP
 Install-WindowsFeature DHCP -IncludeManagementTools
 ##Add DHCP Scope
-Add-DhcpServerv4Scope -Name Scope01 -StartRange 192.168.20.100 -EndRange 192.168.20.149 -SubnetMask 255.255.255.0
+Add-DhcpServerv4Scope -Name Scope01 -StartRange 192.168.1.100 -EndRange 192.168.1.200 -SubnetMask 255.255.255.0
 ##Run to get scope ID
 Get-DhcpServerv4Scope
 ##Configure Scope options
-Set-DhcpServerv4OptionValue -ScopeId 192.168.20.0 -DnsDomain domain.local -DnsServer 192.168.20.10 -Router 192.168.20.254
+Set-DhcpServerv4OptionValue -ScopeId 192.168.1.0 -DnsDomain ReinerChiro.local -DnsServer 192.168.1.2 -Router 192.168.1.254
 ##Add DHCP server to AD and authorize it
 Add-DhcpServerInDC -DnsName host.domain.local -IPAddress 192.168.20.10
+
+## install printer services
+Install-WindowsFeature -Name Print-Server -IncludeManagementTools
--- FolderSize.ps1
+++ FolderSize.ps1
@@ -1,13 +1,20 @@
 ##Parameters
-$source = "\\servers\D$\"
+$source = "\\server\D$\ServerFolders\"
 $destination = "\\rcdc01\D$\"
 
 $folders = 'Quickbooks', 'Users', 'Company'
 
 ##get-childitem -path c:\images | measure-object -Property Length -Sum
 
+Write-Host Source
 foreach ($folder in $folders) {
     $source_folder = $source + $folder
-    $size = get-childitem -path $source_folder | measure-object -Property Length -Sum
-    Write-Host $size
+    $size = (get-childitem -path $source_folder -recurse -file | measure-object -Property Length -Sum).Sum 
+    Write-Host $Folder $size
+    }
+Write-Host Destination
+foreach ($folder in $folders) {
+    $destination_folder = $destination + $folder
+    $size = (get-childitem -path $destination_folder -recurse -file | measure-object -Property Length -Sum).Sum 
+    Write-Host $Folder $size
     }
