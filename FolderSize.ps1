ba52c872eca56c594ff02bc19c6fd351b0464332
kernelfault1024 <141175936+kernelfault1024@users.noreply.github.com>
2023-12-12 13:44:09 -0800

    1212

--- FolderSize.ps1
+++ FolderSize.ps1
@@ -8,14 +8,26 @@ $folders = 'QB', 'IIS'
 ##get-childitem -path c:\images | measure-object -Property Length -Sum
 
 Write-Host Source
+Write-Host ======
+
 foreach ($folder in $folders) {
     $source_folder = $source + $folder
-    $size = (get-childitem -path $source_folder -recurse -file | measure-object -Property Length -Sum).Sum 
-    Write-Host $Folder $size
+    $size = (get-childitem -path $source_folder -recurse -file | measure-object -Property Length -Sum).Sum
+    $count = (get-childitem -path $destination_folder -recurse -file | measure-object -Property Length -Sum).count
+    ##$sizemb = $size / 1024
+    Write-Host $Folder $size KB $count Files
+    ##$sourcearrayname += $source_folder
+    ##$sourcearraysize += $size
     }
+Write-Host
 Write-Host Destination
+Write-Host ======
 foreach ($folder in $folders) {
     $destination_folder = $destination + $folder
-    $size = (get-childitem -path $destination_folder -recurse -file | measure-object -Property Length -Sum).Sum 
-    Write-Host $Folder $size
+    $size = (get-childitem -path $destination_folder -recurse -file | measure-object -Property Length -Sum).Sum
+    $count = (get-childitem -path $destination_folder -recurse -file | measure-object -Property Length -Sum).count
+    ##$sizemb = $size / 1024
+    Write-Host $Folder $size KB $count Files
+    ##$DestArrayName.Add($destination_folder )
+    ##$DestArraySize.Add($size)
     }
