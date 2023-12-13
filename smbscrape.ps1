a0206ae97a3a24b0df66a919b2ab0e88eab00a9e
Buck Lane <bucklane@gmail.com>
2023-12-13 09:46:01 -0800

    12132302

--- smbscrape.ps1
+++ smbscrape.ps1
@@ -1,1 +1,16 @@
-$shares = get-smbshare
+$info = get-computerinfo
+
+$report = "$pwd\Report.txt"
+
+$reportname = 'SMB Share Report'
+
+$compname = 'Computer Name: ' + $info.CsName
+
+$reportname >> $report
+
+
+
+$compname >> $report
+
+$shares = get-smbshare | where-object -property Name -notlike "*$" >> $report
+$adminshares = get-smbshare | where-object -property Name -like "*$" >> $report
